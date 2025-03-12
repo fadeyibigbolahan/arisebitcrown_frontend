0d2df0f896b9dcf2d72c04c44f4cfbdb254035cf
fadeyibigbolahan <fadeyibi26@gmail.com>
2025-03-12 14:13:50 +0100

    build

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/",
   plugins: [react()],
+  build: {
+    outDir: "dist", // Default build output
+  },
   server: {
-    historyApiFallback: true,
+    historyApiFallback: true, // Ensures proper routing in dev
   },
 });
