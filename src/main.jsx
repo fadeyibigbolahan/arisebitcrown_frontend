7c6851faea40403a87513e5f4eb43b5a2cfbfedb
fadeyibigbolahan <fadeyibi26@gmail.com>
2025-03-12 15:04:42 +0100

    im tired

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import { AuthProvider } from "./context/AuthContext.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <App />
       </BrowserRouter>
     </AuthProvider>
