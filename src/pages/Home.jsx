662dfcb4c3aa9f54f6183d72b8976c2fa627de4c
fadeyibigbolahan <fadeyibi26@gmail.com>
2025-03-13 09:13:08 +0100

    okay

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,14 +5,12 @@ import Hero from "../components/Hero";
 
 const Home = () => {
   return (
-    <>
-      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-        <Header />
-        <Hero />
-        <Benefits />
-        <Footer />
-      </div>
-    </>
+    <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+      <Header />
+      <Hero />
+      <Benefits />
+      <Footer />
+    </div>
   );
 };
 
